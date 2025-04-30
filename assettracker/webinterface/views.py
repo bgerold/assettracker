b6ce01294eadd7ecdb8fff8d507864b17689c5a5
B. Gerold <bryang669@gmail.com>
2025-04-29 21:20:06 -0600

    Added asset list view & template

--- assettracker/webinterface/views.py
+++ assettracker/webinterface/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Asset
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    assets = Asset.objects.filter(is_active=True).order_by('name')
+
+    context = {
+        'assets': assets,
+    }
+
+    return render(request, 'webinterface/asset_list.html', context)
+
+def asset(request, asset_id):
+    response = "You're looking at an asset %s."
+    return HttpResponse(response % asset_id)
