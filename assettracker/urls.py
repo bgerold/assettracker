f31442a54a85311a51a94be7741468ac5193d57f
B. Gerold <bryang669@gmail.com>
2025-05-02 21:52:27 -0600

    Direct root URL to webapplication's index page

--- assettracker/urls.py
+++ assettracker/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from webinterface import views as webinterface_views
 
 urlpatterns = [
+    # Direct the root URL to the web interface's index page
+    path('', webinterface_views.index, name='home'),
+
     # URL route for the 'webinterface' Django Application
     path("webinterface/", include("webinterface.urls")),
 
