8dc7ff72112422c434aac70fcbbff7a64dfe46bd
B. Gerold <bryang669@gmail.com>
2025-05-02 21:42:49 -0600

    Cleaned up documentation

--- webinterface/admin.py
+++ webinterface/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Asset, Checkout
 
+# Make the Asset and Checkout models editable in the bultin admin console
 admin.site.register(Asset)
 admin.site.register(Checkout)
